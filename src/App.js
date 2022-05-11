cffcac067bb209dbe4ea464291ed165b2e251acf
printf_cherif <64066992+Maximus203@users.noreply.github.com>
2022-05-11 11:49:18 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="bg-primary text-white p-3">Hello everyone</h1>
+      <h2 className="bg-danger text-white p-3">This is my first react code</h2>
+    </div>
+  );
+}
